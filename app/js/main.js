ad7c1c4c4926f570a93458f7ca10bc896c418b95
makstravm <maks7-2-94@gmail.com>
2021-04-25 02:06:32 +0300

    added media, finish portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,22 @@
 $(function () {
 
+  $('.btn, .menu__link').on('click', function () {
+    $('.btn, .menu').toggleClass('active');
+  });
 
-});
-$('#wrapper').fullpage({
-  anchors: ['page1', 'page2', 'page3', 'page4'],
-  menu: '#menu',
-  responsiveWidth: 1199
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 70) {
+      $(".menu__wrapper").addClass("dark-header");
+    } else {
+      $(".menu__wrapper").removeClass("dark-header");
+    }
+  });
+
+  $('#wrapper').fullpage({
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    menu: '#menu',
+    fitToSection:false,
+    responsiveWidth: 1199
+  });
 });
