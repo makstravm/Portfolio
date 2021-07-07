6b7e02f345b7984fc45f23959831b012af275017
makstravm <maks7-2-94@gmail.com>
2021-07-07 13:11:40 +0300

    added slider in the portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -19,24 +19,49 @@ $(function () {
   $('#wrapper').fullpage({
     anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
     menu: '#menu',
-    fitToSection:false,
+    fitToSection: false,
     responsiveWidth: 1199
   });
 
-  	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
+  $('.portfolio__inner').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: ' <button type="button" class="slick-prev">   <img src="../images/prev.svg" alt="prev images"></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../images/next.svg" alt="next images"></button>',
+    responsive: [
+      {
+        breakpoint: 981,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 571,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
+      }
+    ]
+  });
+
+  //E-mail Ajax Send
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
       $(".form-thank").addClass("active");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 });
